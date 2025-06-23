cc40c666c3aebb0c1b45573ba6e58c60c93408a7
Davi <marcosdavi0755@gmail.com>
2025-06-23 20:17:35 -0300

    alterando o banco

--- database.py
+++ database.py
@@ -1,10 +1,7 @@
-import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./teste.db")
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = "postgresql://usuario:senha@localhost:5432/nome_do_banco"  # Substitua pelos dados reais
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
